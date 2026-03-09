a66a48b47533097e9cf97bf4cbf8f5cdfbbd9c08
Fatih Dişci <fatihdisci@outlook.com>
2026-03-10 00:57:15 +0300

    fix: remove duplicate keystatic api route to fix build collision

--- src/pages/api/keystatic/[...params].ts
+++
@@ -1,5 +0,0 @@
-export const prerender = false;
-import { makeKeystaticAPIRoute } from '@keystatic/astro/api';
-import keystaticConfig from '../../../../keystatic.config';
-
-export const all = makeKeystaticAPIRoute(keystaticConfig);
